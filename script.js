a61960f5cd0c87b5ed5e9812425846354facd87e
Antonio Melena <melenaantonio@gmail.com>
2021-06-07 16:23:31 -0500

    selected each square using queryselectall and added event listener to each

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
 const gameBoard = (() => {
-  const _board = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
-  ];
+  // const _board = [
+  //   ["X", "0", "X"],
+  //   ["0", "X", "X"],
+  //   ["0", "X", "0"],
+  // ];
+  const _board = ["", "", "", "", "", "", "O", "X", "O"];
+  // const _board = new Array(9);
+  for (let i = 0; i <= 8; i++) {
+    const divContent = _board.splice(0, 1);
+    const boardDiv = document.createElement("div");
+    boardDiv.innerText = divContent;
+    document.getElementById("boardContainer").appendChild(boardDiv);
+  }
+
+  const squares = document.querySelectorAll(".boardContainer div")
+  squares.forEach(square => {
+    square.addEventListener("click", clickOutcome)
+  })
+
+  function clickOutcome(e) {
+    // if (!this.innerText) {
+    //   this.innerText = "X";
+    // } else if (this.innerText === "O" || this.innerText === "X") {
+    //   return;
+    // }
+    const squareArray = Array.from(squares)
+    const index = squareArray.indexOf(e.target)
+    console.log(index)
+  }
+})();
+
+const players = (() => {
+  const player = (symbol) => {
+    const playerSymbol = symbol;
+    return playerSymbol;
+  };
+
+  const playerX = player("X");
+  const playerO = player("O");
+  const currentPlayer = playerX;
+
 })();
