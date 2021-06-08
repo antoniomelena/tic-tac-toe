899928d71a1a517182fa015142cbf385e2f9012a
Antonio Melena <melenaantonio@gmail.com>
2021-06-07 19:22:25 -0500

    created boxclicked function to change current player & set innertext to currentplayer

--- script.js
+++ script.js
@@ -1,43 +1,29 @@
 const gameBoard = (() => {
-  // const _board = [
-  //   ["X", "0", "X"],
-  //   ["0", "X", "X"],
-  //   ["0", "X", "0"],
-  // ];
-  const _board = ["", "", "", "", "", "", "O", "X", "O"];
-  // const _board = new Array(9);
-  for (let i = 0; i <= 8; i++) {
-    const divContent = _board.splice(0, 1);
+  const playerX = "X";
+  const playerO = "O";
+  let currentPlayer = playerX;
+
+  const board = new Array(9);
+  for (let i = 0; i <= 8; i += 1) {
+    const divContent = board.splice(0, 1);
     const boardDiv = document.createElement("div");
     boardDiv.innerText = divContent;
     document.getElementById("boardContainer").appendChild(boardDiv);
   }
 
-  const squares = document.querySelectorAll(".boardContainer div")
-  squares.forEach(square => {
-    square.addEventListener("click", clickOutcome)
-  })
+  const squares = document.querySelectorAll(".boardContainer div");
+  function clickedBox(e) {
+    const squareArray = Array.from(squares);
+    const index = squareArray.indexOf(e.target);
 
-  function clickOutcome(e) {
-    // if (!this.innerText) {
-    //   this.innerText = "X";
-    // } else if (this.innerText === "O" || this.innerText === "X") {
-    //   return;
-    // }
-    const squareArray = Array.from(squares)
-    const index = squareArray.indexOf(e.target)
-    console.log(index)
+    if (!board[index]) {
+      board[index] = currentPlayer;
+      e.target.innerText = currentPlayer;
+      currentPlayer = currentPlayer === playerX ? playerO : playerX;
+    }
   }
-})();
 
-const players = (() => {
-  const player = (symbol) => {
-    const playerSymbol = symbol;
-    return playerSymbol;
-  };
-
-  const playerX = player("X");
-  const playerO = player("O");
-  const currentPlayer = playerX;
-
+  squares.forEach((square) => {
+    square.addEventListener("click", clickedBox);
+  });
 })();
